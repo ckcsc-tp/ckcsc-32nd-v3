838cdd2d3fcbfaacff5179249401d1a3a71eeacb
simba <simba.fs@gmail.com>
2019-09-03 21:44:47 +0800

    nothing

---
+++ public/lib/hashes.js
@@ -0,0 +1,1 @@
+../../node_modules/jshashes/hashes.js
--- routes/restful/get.js
+++ routes/restful/get.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+	res.render('index', {title: 'GET'});
+});
+
 ///// get user /////
 
 function user(req, res){
