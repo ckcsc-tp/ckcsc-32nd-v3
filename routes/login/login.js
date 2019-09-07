45147dadf0788697d052b18c5b0345c47a665f32
simba <simba.fs@gmail.com>
2019-09-07 09:00:55 +0800

    login

--- routes/login/login.js
+++ routes/login/login.js
@@ -3,21 +3,26 @@ const router = express.Router();
 
 const user = require('./user-data.json');
 
-router.use('/', (req, res, next) => {
-	if(user.some((x) => x.name === req.body.username)){
-		next();
-	}
-});
-
 router.post('/', (req, res) => {
-	var username = req.body.username;
-	if(user[username].password == req.body.password){
-		res.status(200).send('success');
-		console.log(`${username} login`);
-	}else{
-		console.log(`${username} password error`);
-	}
+	// console.log(`${req.body.username}: \n${req.body.password}`);
+	// res.send(req.body);
+	
+	console.log('/api/GET/login');
 	console.table(req.body);
+	var username = req.body.username;
+	if(username in user){
+		if(user[username].password === req.body.password){
+			console.log(`${username} login`);
+			res.send('login');
+		}else{
+			console.log(`${username} password error`);
+			res.send('password error');
+		}
+	}else{
+		console.log('user not found');
+		res.send('user not found');
+	}
+	
 });
 
 module.exports = router;
