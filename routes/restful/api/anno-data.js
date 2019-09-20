c8b652afcd0cf09d0741ec53dc3b2f5c0eee05e4
simba <simba.fs@gmail.com>
2019-09-20 20:12:03 +0800

    activity

--- routes/restful/api/anno-data.js
+++ routes/restful/api/anno-data.js
@@ -8,26 +8,6 @@
 
 module.exports = [
 	{
-		title: '測試',
-		content: '這是一個測試的公告，請勿理會',
-		author: 'Simba'
-	},
-	{
-		title: '測試2',
-		content: '這是另一個測試的公告，請勿理會',
-		author: 'Simba'
-	},
-	{
-		title: '測試3',
-		content: '這又是一個測試的公告，請勿理會',
-		author: 'Simba'
-	},
-	{
-		title: '公告系統測試',
-		content: '如果看到這個公告，代表公告系統完成了！！',
-		author: 'Simba'
-	},
-	{
 		title: '注意',
 		content: '謝謝你的注意',
 		author: 'tino'
