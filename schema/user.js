70eaf56b1b2d5cdd32e4cee666c213dcaf14166c
simba <simba.fs@gmail.com>
2019-11-24 22:52:34 +0800

    add 2048

--- schema/user.js
+++ schema/user.js
@@ -1,9 +1,38 @@
 const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
+
+var register = Joi.object({
+	name: Joi
+		.string()
+		.min(3)
+		.required(),
+	password: Joi
+		.string()
+		.min(10)
+		.required(),
+	email: Joi
+		.string()
+		.min(6)
+		.required()
+		.email()
+});
+
+var login = Joi.object({
+	email: Joi
+		.string()
+		.min(6)
+		.required()
+		.email(),
+	password: Joi
+		.string()
+		.min(10)
+		.required()
+});
 
 var UserSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		min: 6,
+		min: 3,
 		max: 225,
 		required: true 
 	},
@@ -18,7 +47,16 @@ var UserSchema = new mongoose.Schema({
 		min: 6,
 		max: 225,
 		required: true 
+	},
+	date: {
+		type: Date,
+		default: Date.now
 	}
+
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = {
+	UserMoule: mongoose.model('User', UserSchema),
+	registerValidate: (body) => register.validate(body),
+	loginValidate: (body) => login.validate(body)
+}
